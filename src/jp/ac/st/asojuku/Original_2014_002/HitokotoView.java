6bf452f8d8d8990e80b4f6b6ac9b5f4899352f5b
student <student@PC111S40.fgakusei.local>
2014-06-11 11:11:28 +0900

    2014/06/11

--- src/jp/ac/st/asojuku/Original_2014_002/HitokotoView.java
+++ src/jp/ac/st/asojuku/Original_2014_002/HitokotoView.java
@@ -1,6 +1,8 @@
 package jp.ac.st.asojuku.Original_2014_002;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 
 public class HitokotoView extends Activity {
@@ -11,6 +13,20 @@ public class HitokotoView extends Activity {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.hitokotoview);
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onResume();
+
+		//画面(Activity)に渡されたインテントを取得
+		Intent intent = this.getIntent();
+		//intentから、Extaraに混入させた値をキーワードで取得
+		String strHitokoto = intent.getStringExtra("hitokoto");
+		//取得したStringを、TxtViewにセット
+		TextView txvHITOKOTO = (TextView)findViewById(R.id.txthitokoto);
+		txvHITOKOTO.setText(strHitokoto);
 	}
 
 }
--- src/jp/ac/st/asojuku/Original_2014_002/MainActivity.java
+++ src/jp/ac/st/asojuku/Original_2014_002/MainActivity.java
@@ -2,12 +2,18 @@ package jp.ac.st.asojuku.Original_2014_002;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity  implements View.OnClickListener{
+
+	SQLiteDatabase sdb = null;
+	MySQLiteOpenHelper helper = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +32,17 @@ public class MainActivity extends Activity  implements View.OnClickListener{
 		btnMente.setOnClickListener(this);
 		Button btntyekku =(Button)findViewById(R.id.btntyekku);
 		btntyekku.setOnClickListener(this);
+
+		//クラスのフィールド変数がNULLなら、データベース空間オープン
+		if(sdb == null){
+			helper = new MySQLiteOpenHelper(getApplicationContext());
+		}
+		try{
+			sdb = helper.getWritableDatabase();
+		}catch(SQLiteException e){
+			//異常終了
+			return;
+		}
 	}
 
 	@Override
@@ -34,13 +51,30 @@ public class MainActivity extends Activity  implements View.OnClickListener{
 		// TODO 自動生成されたメソッド・スタブ
 		switch(v.getId()){
 		case R.id.btnSubmit:
+			//エディットテキストからの入力内容を取り出す
+			EditText etv = (EditText)findViewById(R.id.edtHitokoto);
+			String inputMsg = etv.getText().toString();
+
+			//inputMsgがnullでない、かつ、空でない場合のみ、if文内を実行
+			if(inputMsg!=null && !inputMsg.isEmpty()){
+				//MySQLiteOpenHelperのインサートメソッドを呼び出し
+				helper.insertHitokoto(sdb, inputMsg);
+			}
+			//入力欄をクリア
+			etv.setText("");
 			break;
 		case R.id.btnMente:
 			intent = new Intent(MainActivity.this, Hensyu.class);
 			startActivity(intent);
 			break;
 		case R.id.btntyekku:
+			//MySQLiteOpenHeloperのセレクト一言メソッドを呼び出して一言をランダムに習得
+			String strHitokoto = helper.selectRandomHitokoto(sdb);
+			//インテントのインスタンス生成
 			intent = new Intent(MainActivity.this, HitokotoView.class);
+			//インテントに一言を混入
+			intent.putExtra("hitokoto", strHitokoto);
+			//次画面のアクティビティ起動
 			startActivity(intent);
 			break;
 		}
